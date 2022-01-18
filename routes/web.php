206373270303f992cb83bc4f5ef0f73762ffae56
VitorBrigoni <93330237+VitorBrigoni@users.noreply.github.com>
2022-01-18 09:45:29 -0300

    Criação do Blog

---
+++ routes/web.php
@@ -0,0 +1,85 @@
+<?php
+
+use App\Http\Controllers\ProdutosController;
+use App\Http\Controllers\UsuariosController;
+use App\Http\Controllers\RecadosController;
+use App\Http\Controllers\BlogController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', ['pagina' => 'home']);
+})->name('home');
+
+Route::get('produtos', [ProdutosController::class, 'index'])->name('produtos');
+
+Route::get('/produtos/inserir', [ProdutosController::class, 'create'])->name('produtos.inserir');
+
+Route::post('/produtos/inserir', [ProdutosController::class, 'insert'])->name('produtos.gravar');
+
+Route::get('/produtos/{prod}', [ProdutosController::class, 'show'])->name('produtos.show');
+
+Route::get('/produtos/{prod}/editar', [ProdutosController::class, 'edit'])->name('produtos.edit');
+
+Route::put('/produtos/{prod}/editar', [ProdutosController::class, 'update'])->name('produtos.update');
+
+Route::get('/produtos/{prod}/apagar', [ProdutosController::class, 'remove'])->name('produtos.remove');
+
+Route::delete('/produtos/{prod}/apagar', [ProdutosController::class, 'delete'])->name('produtos.delete');
+
+
+
+Route::get('usuarios', [UsuariosController::class, 'index'])->name('usuarios.index');
+
+Route::prefix('usuarios')->group(function() {
+    
+    Route::get('/inserir', [UsuariosController::class, 'create'])->name('usuarios.inserir');
+    Route::post('/inserir', [UsuariosController::class, 'insert'])->name('usuarios.gravar');
+
+});
+// Criação das rotas usadas pelos recados
+Route::get('recados', [RecadosController::class, 'index'])->name('recados');
+
+Route::prefix('recados')->group(function() {
+    
+    Route::get('/inserir', [RecadosController::class, 'create'])->name('recados.inserir');
+    Route::post('/inserir', [RecadosController::class, 'insert'])->name('recados.gravar');
+
+});
+
+Route::get('/recados/{reca}/editar', [RecadosController::class, 'edit'])->name('recados.edit');
+
+Route::put('/recados/{reca}/editar', [RecadosController::class, 'update'])->name('recados.update');
+
+Route::get('/recados/{reca}/apagar', [RecadosController::class, 'remove'])->name('recados.remove');
+
+Route::delete('/recados/{reca}/apagar', [RecadosController::class, 'delete'])->name('recados.delete');
+
+
+
+Route::get('/login', [UsuariosController::class, 'login'])->name('login');
+Route::post('/login', [UsuariosController::class, 'login']);
+
+Route::get('/logout', [UsuariosController::class, 'logout'])->name('logout');
+
+Route::get('/produtos/{prod}/imagens', [ProdutosController::class, 'images'])->name('image.get');
+
+Route::post('/produtos/imagens', [ProdutosController::class, 'images'])->name('image.create');
+
+Route::get('blog', [BlogController::class, 'index'])->name('blog');
+
+Route::get('/blog/inserir', [BlogController::class, 'create'])->name('blog.inserir');
+
+Route::post('/blog/inserir', [BlogController::class, 'store'])->name('blog.gravar');
+
+Route::get('/blog/{post}', [BlogController::class, 'show'])->name('blog.show');
